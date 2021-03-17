9e93deb12abc09b70cf40de7725ebaecd4ef8927
marko-dunatov <marko-dunatov@users.noreply.github.com>
2021-03-16 23:07:09 -0400

    add the app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {SocialMediaComponent} from './social.media/social.media.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PublicationsComponent } from './publications/publications.component';
+import { ResearchComponent } from './research/research.component';
+import { ContactComponent } from './contact/contact.component';
+import { TeachingComponent } from './teaching/teaching.component';
+import { AboutComponent } from './about/about.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialMediaComponent,
+    WelcomeComponent,
+    PublicationsComponent,
+    ResearchComponent,
+    ContactComponent,
+    TeachingComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
