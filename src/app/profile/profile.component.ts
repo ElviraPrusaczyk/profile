68d228a73dca13fdd6152d244cb34faaf8d04330
marko-dunatov <mark.dunatov@outlook.com>
2021-03-22 19:04:37 -0400

    add content for each page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { fadeInAnimation } from '../_animations/index';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  animations: [fadeInAnimation],
+  host: { '[@fadeInAnimation]': '' }
 })
+
 export class ProfileComponent implements OnInit {
 
   firstName = 'Elvira';
